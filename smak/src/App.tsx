ff08cf0c5c6203a5cbbb8b2b2a711af21eafcf4b
Jesper <jw_jw@hotmail.com>
2025-10-15 09:42:14 +0200

    add quarry for desktop trigger

--- smak/src/App.tsx
+++ smak/src/App.tsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Main from "./partials/Main";
 import Header from "./partials/Header";
 import Footer from "./partials/Footer";
+import DesktopPage from "./pages/desktop/DesktopPage";
+import config from "./config/Config";
 
 export default function App() {
   useLocation();
   window.scrollTo({ top: 0, left: 0, behavior: "instant" });
 
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const checkIsDesktop = () => {
+      setIsDesktop(window.innerWidth >= config.widthBreakpointDesktop);
+    };
+    checkIsDesktop();
+    window.addEventListener('resize', checkIsDesktop);
+
+    return () => window.removeEventListener('resize', checkIsDesktop);
+  }, []);
+
   return <>
-    <Header />
-    <Main />
-    <Footer />
+    {isDesktop
+      ? (<DesktopPage />)
+      : (<>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+      )}
   </>;
 };
---
+++ smak/src/pages/desktop/DesktopPage.tsx
@@ -0,0 +1,13 @@
+
+export default function DesktopPage() {
+  return (
+    <div className="d-flex justify-content-center align-items-center flex-column">
+      <h1>Desktop</h1>
+      <h1>Desktop</h1>
+      <h1>Desktop</h1>
+      <h1>Desktop</h1>
+      <h1>Desktop</h1>
+      <h1>Desktop</h1>
+    </div>
+  )
+}
