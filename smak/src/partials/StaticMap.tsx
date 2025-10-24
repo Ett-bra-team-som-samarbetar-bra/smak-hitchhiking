e5a2bf22a64067c1941454a4aa3917b48834b631
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-10-24 11:12:14 +0200

    started creating a hook for fetching routes

--- smak/src/partials/StaticMap.tsx
+++ smak/src/partials/StaticMap.tsx
@@ -1,12 +1,32 @@
 import { useEffect, useState } from "react";
 
+const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+
 export default function StaticMap() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchMap = async () => {
+      const geojson = {
+        type: "Feature",
+        properties: {
+          stroke: "#ff0000",
+          "stroke-width": 4,
+          "stroke-opacity": 0.8,
+        },
+        geometry: {
+          type: "LineString",
+          coordinates: [
+            [18.0686, 59.3293],
+            [15.6214, 58.4109],
+          ],
+        },
+      };
+
+      const encoded = encodeURIComponent(JSON.stringify(geojson));
+
       const res = await fetch(
-        `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/[-121.8742,37.6292,-121.462,37.8465]/300x200?access_token=pk.eyJ1Ijoic2FtYWttZWRzbWFrIiwiYSI6ImNtZ3lzc3YydDF2c2wyanI3MnozeHYwdXYifQ.7bzxGIbFwKO7Hc7_I1fnLw`
+        `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/geojson(${encoded})/auto/600x400?padding=100&access_token=${MAPBOX_TOKEN}`
       );
       const blob = await res.blob();
       const url = URL.createObjectURL(blob);
