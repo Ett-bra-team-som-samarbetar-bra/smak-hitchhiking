bb55b2af132fb1f7a8ac2f959c260e1e0c7cce0d
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-10-27 16:12:00 +0100

    added baseline for implementing payments

--- smak/src/pages/debugPages/debugPage.tsx
+++ smak/src/pages/debugPages/debugPage.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 import routes from "../../routes";
 
 export default function DebugPagePage() {
-  const debugRoutes = routes.filter(route =>
-    ["/joggus", "/kalv", "/tungis"].includes(route.path)
+  const debugRoutes = routes.filter((route) =>
+    ["/joggus", "/kalv", "/tungis", "/payment"].includes(route.path)
   );
 
   return (
@@ -12,22 +12,21 @@ export default function DebugPagePage() {
         className="position-absolute w-100 h-100"
         style={{
           backgroundImage: 'url("/images/development/kalv.png")',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
           opacity: 0.2,
-          zIndex: -1
+          zIndex: -1,
         }}
       />
 
       <h1 className="mb-4 text-danger fw-bold">Debug Ba</h1>
 
-      {debugRoutes.map(route => (
-        <NavLink
-          key={route.path}
-          to={route.path}
-          className="mb-2 fw-bold">
-          <i className={`bi bi-${route.icon} text-center nav-icon-size text-danger me-2`} />
+      {debugRoutes.map((route) => (
+        <NavLink key={route.path} to={route.path} className="mb-2 fw-bold">
+          <i
+            className={`bi bi-${route.icon} text-center nav-icon-size text-danger me-2`}
+          />
           {route.path}
         </NavLink>
       ))}
