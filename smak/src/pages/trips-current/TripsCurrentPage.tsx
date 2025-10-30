1d79f9e0301b29c05f21c25c2a5a964af7a1f699
JockeBjers <joakim.bjerselius@hotmail.com>
2025-10-30 13:51:01 +0100

    add bootstrap accordion to show/hide sections in current trips

--- smak/src/pages/trips-current/TripsCurrentPage.tsx
+++ smak/src/pages/trips-current/TripsCurrentPage.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import Row from "react-bootstrap/esm/Row";
 import TripCardBig from "../../components/trip/TripCardBig";
 import SmakContact from "../../components/SmakContact";
 import { getAllTrips, getMockUsers, getMockUser } from "../../utils/MockData";
 /* import { useAuth } from "../../hooks/useAuth"; */
 import CarModal from "../profile/CarModal";
 import { useNavigate } from "react-router-dom";
+import Accordion from "react-bootstrap/Accordion";
 
 export default function TripsCurrentPage() {
   const currentUser = getMockUser();
@@ -66,73 +66,89 @@ export default function TripsCurrentPage() {
         byt till: {isDriver ? "Passagerare" : "Förare"}
       </button>
 
-      <Row className="d-flex align-items-center mb-4">
-        <h2 className="mb-0">{isDriver ? "Förare" : "Passagerare"} <i className="bi bi-chevron-down"></i></h2>
-      </Row>
+      <Accordion defaultActiveKey={["0", "1", "2"]} alwaysOpen className="mb-4 custom-accordion">
 
-      <TripCardBig
-        startTime={firstTrip.startTime}
-        endTime={firstTrip.endTime}
-        startCity={firstTrip.startCity}
-        endCity={firstTrip.endCity}
-        distance={firstTrip.distance}
-        date={firstTrip.date}
-        firstName={currentUser.firstName}
-        lastName={currentUser.lastName}
-        profileImage={currentUser.profileImage}
-        rating={currentUser.rating}
-        vehicleInfo="volvo v60"
-        numOfSeats="3"
-        onCarClick={handleCarClick}
-        onUserClick={() => handleUserClick(currentUser)}
-      />
+        <Accordion.Item eventKey="0">
+          <Accordion.Header className="py-1">
+            <span className="fs-5 fw-semibold">Förare</span>
+            <i className="bi bi-caret-down-fill ms-2"></i>
+          </Accordion.Header>
+          <Accordion.Body>
+            <TripCardBig
+              startTime={firstTrip.startTime}
+              endTime={firstTrip.endTime}
+              startCity={firstTrip.startCity}
+              endCity={firstTrip.endCity}
+              distance={firstTrip.distance}
+              date={firstTrip.date}
+              firstName={currentUser.firstName}
+              lastName={currentUser.lastName}
+              profileImage={currentUser.profileImage}
+              rating={currentUser.rating}
+              vehicleInfo="Volvo V60"
+              numOfSeats="3"
+              onCarClick={handleCarClick}
+              onUserClick={() => handleUserClick(currentUser)}
+            />
+          </Accordion.Body>
+        </Accordion.Item>
 
-      <Row className="d-flex align-items-center my-4">
-        <h2 className="mb-0">{isDriver ? "Passagerare" : "Övriga passagerare"} <i className="bi bi-chevron-down"></i></h2>
-      </Row>
-      {passengers.length === 0 && (
-        <p className="text-black-50">Inga passagerare just nu.</p>
-      )}
-      <div className="d-flex flex-column gap-3">
-        {passengers.map((user, index) => (
-          <SmakContact
-            key={index}
-            user={user}
-            isDriver={isDriver}
-            isAddedToTrip={true}
-            onClick={() => handleUserClick(user)}
-            onRemove={isDriver ? () => handleRemovePassenger(user) : undefined}
-          />
-        ))}
-      </div>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header className="py-1">
+            <span className="fs-5 fw-semibold">{isDriver ? "Passagerare" : "Övriga passagerare"}</span>
+            <i className="bi bi-caret-down-fill ms-2"></i>
+          </Accordion.Header>
+          <Accordion.Body>
+            {passengers.length === 0 ? (
+              <p className="text-black-50">Inga passagerare just nu.</p>
+            ) : (
+              <div className="d-flex flex-column gap-3">
+                {passengers.map((user, index) => (
+                  <SmakContact
+                    key={index}
+                    user={user}
+                    isDriver={isDriver}
+                    isAddedToTrip={true}
+                    onClick={() => handleUserClick(user)}
+                    onRemove={isDriver ? () => handleRemovePassenger(user) : undefined}
+                  />
+                ))}
+              </div>
+            )}
+          </Accordion.Body>
+        </Accordion.Item>
 
-      {isDriver && (
-        <>
-          <Row className="d-flex align-items-center my-4">
-            <h2 className="mb-0">Förfrågningar <i className="bi bi-chevron-down"></i></h2>
-          </Row>
-          {
-            (requests.length === 0 && (
-              <p className="text-black-50">Inga förfrågningar just nu.</p>
-            ))
-          }
-          <div className="d-flex flex-column gap-3 mb-5">
-            {requests.map((user, index) => (
-              <SmakContact
-                key={index}
-                user={user}
-                isDriver={true}
-                isAddedToTrip={false}
-                onClick={() => handleUserClick(user)}
-                onAccept={() => handleAcceptRequest(user)}
-                onDeny={() => handleDenyRequest(user)}
-              />
-            ))}
-          </div>
-        </>
-      )}
+        {isDriver && (
+          <Accordion.Item eventKey="2">
+            <Accordion.Header className="py-1">
+              <span className="fs-5 fw-semibold">Förfrågningar</span>
+              <i className="bi bi-caret-down-fill ms-2"></i>
+            </Accordion.Header>
+            <Accordion.Body>
+              {requests.length === 0 ? (
+                <p className="text-black-50">Inga förfrågningar just nu.</p>
+              ) : (
+                <div className="d-flex flex-column gap-3 mb-3">
+                  {requests.map((user, index) => (
+                    <SmakContact
+                      key={index}
+                      user={user}
+                      isDriver={true}
+                      isAddedToTrip={false}
+                      onClick={() => handleUserClick(user)}
+                      onAccept={() => handleAcceptRequest(user)}
+                      onDeny={() => handleDenyRequest(user)}
+                    />
+                  ))}
+                </div>
+              )}
+            </Accordion.Body>
+          </Accordion.Item>
+        )}
+      </Accordion>
 
       <CarModal
+        title="Fordon"
         show={showCarModal}
         onClose={handleCloseCarModal}
         payload={carPayload}
