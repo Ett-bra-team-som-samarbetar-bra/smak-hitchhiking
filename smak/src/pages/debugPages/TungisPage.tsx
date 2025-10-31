11248a34612e3f6cb145c747fc0542f35be3682b
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-10-31 10:21:53 +0100

    finished functionality for profilepictures on userSettings

--- smak/src/pages/debugPages/TungisPage.tsx
+++ smak/src/pages/debugPages/TungisPage.tsx
@@ -1,10 +1,7 @@
-import FileUpload from '../../components/fileUpload/FileUpload';
-
 export default function TungisPage() {
   return (
     <>
       <h1>Hej</h1>
-      <FileUpload />
     </>
   );
 }
