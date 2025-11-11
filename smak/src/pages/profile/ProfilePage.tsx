51e7278c81c0447caac5ca7b4895f2d005ee7fa9
Jesper <jw_jw@hotmail.com>
2025-11-11 08:11:45 +0100

    debug mediauploader

--- smak/src/pages/profile/ProfilePage.tsx
+++ smak/src/pages/profile/ProfilePage.tsx
@@ -212,16 +212,25 @@ export default function ProfilePage() {
         }
 
         await refreshUser();
-
         setUserPayload({ user: savedUser });
         setShowUserModal(false);
+
+
+        // todo
       } catch (error) {
         showAlert({
+          message: `${error instanceof Error ? error.message : String(error)}`,
+          backgroundColor: "danger",
+          textColor: "white",
+          duration: 8000,
+        });
+
+        /* showAlert({
           message: "Kunde inte spara ändringarna. Försök igen.",
           backgroundColor: "danger",
           textColor: "white",
           duration: 5000,
-        });
+        }); */
       }
     }
   }
